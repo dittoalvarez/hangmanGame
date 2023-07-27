8d111d3459983580d592f464e1c1ed02015bf305
ditto <dittoalvarez@gmail.com>
2023-07-27 12:59:01 +0800

    added main image of the game

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
+import Words from ".//assets/wordList.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("test");
-  return <h1>{wordToGuess}</h1>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return Words[Math.floor(Math.random() * Words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>LOSE WIN</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ hangman/src/components/HangmanDrawing.tsx
@@ -0,0 +1,37 @@
+export const HangmanDrawing = () => {
+  return (
+    <div
+      style={{
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          height: "50px",
+          width: "10px",
+          background: "black",
+          top: 0,
+          right: 0,
+          position: "absolute",
+        }}
+      />
+      <div
+        style={{
+          height: "10px",
+          width: "200px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div style={{ height: "10px", width: "250px", background: "black" }} />
+    </div>
+  );
+};
