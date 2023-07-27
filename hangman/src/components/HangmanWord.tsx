99b90bbab1d9b28f7550b58e1039a20ed04c713e
ditto <dittoalvarez@gmail.com>
2023-07-27 22:41:33 +0800

    added the word section display

--- hangman/src/components/HangmanWord.tsx
+++ hangman/src/components/HangmanWord.tsx
@@ -1,1 +1,30 @@
-export const HangmanWord = () => {};
+export const HangmanWord = () => {
+  const word = "test";
+  const guessedLetters = ["t", "s"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+};
