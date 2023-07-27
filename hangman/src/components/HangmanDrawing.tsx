2c5ee558f7562fac7466366c524c255f69ba5808
ditto <dittoalvarez@gmail.com>
2023-07-27 22:22:21 +0800

    completed the hangman image

--- hangman/src/components/HangmanDrawing.tsx
+++ hangman/src/components/HangmanDrawing.tsx
@@ -1,3 +1,86 @@
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid red",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+);
+
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      position: "absolute",
+      background: "red",
+      top: "120px",
+      right: "0px",
+    }}
+  />
+);
+const RIGHT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      position: "absolute",
+      background: "red",
+      top: "150px",
+      right: "-100px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+const LEFT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      position: "absolute",
+      background: "red",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+const RIGHT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      position: "absolute",
+      background: "red",
+      top: "210px",
+      right: "-90px",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+const LEFT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      position: "absolute",
+      background: "red",
+      top: "210px",
+      right: "0px",
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+
 export const HangmanDrawing = () => {
   return (
     <div
@@ -5,6 +88,12 @@ export const HangmanDrawing = () => {
         position: "relative",
       }}
     >
+      {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
       <div
         style={{
           height: "50px",
